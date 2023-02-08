e04f7aff10784a19d3911dc4fa8d5c2239396188
sspinner224 <sspinner@quinnipiac.edu>
2023-02-07 19:38:44 -0500

    adds 2 numbers

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Calculator{
     
     public static void main(String[] args) {
-    
-        System.out.println("Hello World");
+        int num1;
+        int num2;
+        int sum;
+
+        System.out.println("Please enter two whole numbers");
+        Scanner scanner = new Scanner(System.in);
+
+        num1 = scanner.nextInt();
+        num2 = scanner.nextInt();
+        sum = num1+num2;
+
+        System.out.println(sum);
+
+
     }  
 
 }
