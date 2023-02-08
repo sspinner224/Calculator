9f183348d0bb4ec19100bb01975b9f9dba042c7e
sspinner224 <sspinner@quinnipiac.edu>
2023-02-08 13:15:47 -0500

    option for subtraction

--- Calculator.java
+++ Calculator.java
@@ -5,18 +5,36 @@ public class Calculator{
     public static void main(String[] args) {
         int num1;
         int num2;
-        int sum;
+        char operation;
+        int res;
 
         System.out.println("Please enter two whole numbers");
         Scanner scanner = new Scanner(System.in);
 
         num1 = scanner.nextInt();
         num2 = scanner.nextInt();
-        sum = num1+num2;
 
-        System.out.println(sum);
+        System.out.println("Would you like to add or subtract?");
+
+        scanner = new Scanner(System.in);
+        operation = scanner.next().charAt(0);
+
+        if(operation == '+') {
+            res = num1+num2;  
+            System.out.println(res);  
+        } else if (operation == '-'){
+            res = num1-num2;
+            System.out.println(res);
+        } else {
+            System.out.println("Please type '+' or '-'");
+        }
+        
+            
+        
 
 
-    }  
+    }
+
+   
 
 }
